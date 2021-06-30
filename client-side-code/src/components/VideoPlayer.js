4afb8a50dd027204140d25c7297b9f6913ab0824
Shushi2023 <yashi.shukla2023@gmail.com>
2021-07-01 05:27:39 +0530

    Changed the structure of video call

--- client-side-code/src/components/VideoPlayer.js
+++ client-side-code/src/components/VideoPlayer.js
@@ -1,59 +1,37 @@
 import React, { useContext, useState } from 'react';
-import { Grid, Typography, Paper, makeStyles, Button } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { SocketContext } from '../contexts/Servercontex';
-
-const useStyles = makeStyles((theme) => ({
-  video: {
-    width: '550px',
-    [theme.breakpoints.down('xs')]: {
-      width: '300px',
-    },
-  },
-  gridContainer: {
-    justifyContent: 'center',
-    [theme.breakpoints.down('xs')]: {
-      flexDirection: 'column',
-    },
-  },
-  paper: {
-    padding: '10px',
-    border: '2px solid black',
-    margin: '10px',
-  },
-}));
+import './styles.css';
+import { Navigation } from './navigation';
 
 const VideoPlayer = () => {
   const { name, callAccepted, myVideo, userVideo, callEnded, stream, call, shareScreen } = useContext(SocketContext);
-  const classes = useStyles();
   const [mute, setMute] = useState(true);
-
-  
   
   return (
-    <Grid container className={classes.gridContainer}>
-      {stream && (
-        <Paper className={classes.paper}>
-          <Grid item xs={12} md={6}>
-            <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
-            <video playsInline muted={mute} ref={myVideo} autoPlay className={classes.video} />
-          </Grid>
-        </Paper>
-      )}
-      {callAccepted && !callEnded && (
-        <Paper className={classes.paper}>
-          <Grid item xs={12} md={6}>
-            <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
-            <video playsInline ref={userVideo} autoPlay className={classes.video} />
-          </Grid>
-          <Button style = {{fontSize : "15px"}} variant="contained" color="primary" onClick={() => setMute(!mute)}>
-            Mute
-          </Button>
-          <Button style = {{fontSize : "15px"}} variant="contained" color="primary" onClick={shareScreen}>
-            Share Screen
-          </Button>
-        </Paper>
-      )}
-    </Grid>
+    <>
+      {(!callAccepted || callEnded) && <Navigation />}
+      <div className="container" style = {{marginLeft : "0px", marginRight : "0px"}}>
+        {callAccepted && !callEnded && (<div className="UserVideo" style = {{width : "100%", height : "500px", overflow : "hidden"}}><video style = {{width : "100%"}} muted={mute} ref={userVideo} autoPlay/></div>)}
+        {(callAccepted && !callEnded) ? 
+        <div className="Secondary-Video" style = {{backgroundColor : "black"}}>
+          {stream && (<div className="MyVideo" style = {{width : "100%", height : "166px", overflow : "hidden"}}>
+            <video style = {{width : "100%"}} className = "videoInsert" ref={myVideo} autoPlay/>
+          </div>)}
+        </div> :
+        <div className="Secondary-Video">
+          {stream && (<div className="MyVideo" style = {{width : "100%", height : "166px", overflow : "hidden"}}>
+            <video style = {{width : "100%"}} className = "videoInsert" ref={myVideo} autoPlay/>
+          </div>)}
+        </div>}
+      </div>
+      {callAccepted && !callEnded && (<Button style = {{fontSize : "15px"}} variant="contained" color="primary" onClick={() => setMute(!mute)}>
+        Mute
+      </Button>)}
+      {callAccepted && !callEnded && (<Button style = {{fontSize : "15px"}} variant="contained" color="primary" onClick={shareScreen}>
+        Share Screen
+      </Button>)}
+    </>
   );
 };
 
